8cefb9d46bb227363377aa736fa9c7d7bb62ee1a
rostog <rostogiorgi@gmail.com>
2019-02-13 12:42:43 +0100

    added new snapshot tests for error messages

--- packages/expect/src/__tests__/matchers.test.js
+++ packages/expect/src/__tests__/matchers.test.js
@@ -2105,6 +2105,7 @@ describe('toMatchObject()', () => {
     });
   });
 
+<<<<<<< HEAD
   it('does not match properties up in the prototype chain', () => {
     const a = {};
     a.ref = a;
@@ -2118,6 +2119,18 @@ describe('toMatchObject()', () => {
     jestExpect(b).not.toMatchObject(matcher);
     expect(() =>
       jestExpect(b).toMatchObject(matcher),
+=======
+  test('failure message matches the expected snapshot', () => {
+    expect(() =>
+      jestExpect({
+        a: 'a',
+        b: 'b',
+        c: 'c',
+      }).toMatchObject({
+        a: 'x',
+        b: expect.any(String),
+      }),
+>>>>>>> 4576618b6... added new snapshot tests for error messages
     ).toThrowErrorMatchingSnapshot();
   });
 });
